75d0a942b90a70e652148de6b96b5bf410b62062
DoctorHeyde <alexanderrasmuss@gmail.com>
2026-03-17 09:25:49 +0100

    Fix

--- fake_info/src/main/java/com/example/demo/FakeInfo.java
+++ fake_info/src/main/java/com/example/demo/FakeInfo.java
@@ -1,3 +1,5 @@
+package com.example.demo;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
--- fake_info/src/main/java/com/example/demo/Town.java
+++ fake_info/src/main/java/com/example/demo/Town.java
@@ -1,3 +1,5 @@
+package com.example.demo;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
