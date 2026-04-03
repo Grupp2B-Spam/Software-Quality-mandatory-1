a767efdcff91768ebfc3d6e83d6d3f6f37d716fe
Mikkel Olsen <molybdenum93@gmail.com>
2026-04-04 00:34:39 +0200

    Add api tests

--- fake_info_e2e_tests/playwright.config.ts
+++ fake_info_e2e_tests/playwright.config.ts
@@ -26,7 +26,6 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'http://localhost:3000',
     video:{
       mode: 'retain-on-failure', // should only be necessary if test fails. Argument can be made for first retry though
       size: { width: 640, height: 480 }
@@ -42,18 +41,35 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+        name: 'api',
+        testDir: './tests/endpoint_tests',
+        use: {
+            baseURL: 'http://localhost:8081',
+        },
     },
-
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+        name: 'chromium',
+        testDir: './tests/e2e_tests',
+        use: { 
+            ...devices['Desktop Chrome'],
+            baseURL: 'http://localhost:3000',
+        },
     },
-
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+        name: 'firefox',
+        testDir: './tests/e2e_tests',
+        use: { 
+            ...devices['Desktop Firefox'],
+            baseURL: 'http://localhost:3000',
+        },
+    },
+    {
+        name: 'webkit',
+        testDir: './tests/e2e_tests',
+        use: { 
+            ...devices['Desktop Safari'],
+            baseURL: 'http://localhost:3000',
+        },
     },
 
     /* Test against mobile viewports. */
@@ -75,6 +91,7 @@ export default defineConfig({
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
+
   ],
 
   /* Run your local dev server before starting the tests */
