f46aa9db863728873f02268fe54f4f8c9cfbfa46
Mikkel Olsen <115707577+Selmerr@users.noreply.github.com>
2026-04-06 10:33:58 +0200

    Update FakeInfoTest.java

--- fake_info/src/test/java/com/example/demo/FakeInfoTest.java
+++ fake_info/src/test/java/com/example/demo/FakeInfoTest.java
@@ -72,7 +72,38 @@ class FakeInfoTest {
         String birthDate = personData.get("birthDate").toString();
         int lastCPRIndex = Character.getNumericValue(cpr.charAt(cpr.length()-1));
 
-        System.out.println(birthDate);
+        int cprDate = Integer.parseInt(cpr.substring(0,2));
+        int cprMonth = Integer.parseInt(cpr.substring(2,4));
+        int cprYear = Integer.parseInt(cpr.substring(4,6));
+
+        //Assertions
+        assertNotNull(personData);
+
+        //CPR needs to be only 10 character.
+        assertTrue(cpr.matches("[0-9]{10}"), "Expected CPR to be 10 numbers, but got: " + cpr + " \nCpr length: " + cpr.length());
+        assertTrue(gender.equals("male") || gender.equals("female"));
+
+        //Name can't contain the numbers of characters in the regex
+        assertFalse(firstName.matches("[0-9,!@%^$&#]"));
+        assertFalse(lastName.matches("[0-9,!@%^$&#]"));
+
+        //last digit of cpr should be odd for males and even for females
+        if (gender.equals("female")) {
+            assertEquals(0,lastCPRIndex % 2, "Expected "+firstName+" cpr to be even, but was "+cpr);
+
+        }
+
+        if (gender.equals("male")) {
+            assertEquals(1,lastCPRIndex % 2, "Expected "+firstName+" cpr to be odd, but was "+cpr);
+        }
+
+            //First 2 digits should be the day
+        assertTrue(32> cprDate && cprDate > 0, "Expected date to be valid, but was "+cprDate);
+        //Third and fourth digit should be the month
+        assertTrue(12>= cprMonth && cprMonth > 0, "Expected month to be valid, but was "+cprMonth);
+        //Fifth and sixth digit should be the year
+        assertTrue(100 > cprYear && cprYear >= 0, "Expected year to be valid, but was "+cprYear);
+
         assertTrue(birthDate.matches("\\d{4}-\\d{2}-\\d{2}"), "Birthdate is expected to be YYY-MM-DD, but was "+birthDate);
     }
 }
